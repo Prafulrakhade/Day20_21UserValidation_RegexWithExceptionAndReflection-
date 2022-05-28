766d1a85df161c4a78aed8f19dbda9261826af3b
Praful <prafulrakhade02@gmail.com>
2022-05-28 15:17:59 +0530

    [praful] Added : uc_2_Proram file

--- MsTestUserRegistration/UserTest.cs
+++ MsTestUserRegistration/UserTest.cs
@@ -35,5 +35,27 @@ namespace MsTestUserRegistration
                 Assert.AreEqual(expected, ex.Message);
             }
         }
+
+        [TestMethod]
+        //Checking for multiple last names
+        [DataRow("Rakhade", "Input is valid")]
+        [DataRow("ra", "Input is not valid")]
+        [DataRow("rakhade", "Input is not valid")]
+        [DataRow("", "Input should not be empty")]
+        [DataRow(null, "Input should not be null")]
+        public void GivenLastNameValidation(string lastName, string expected) // Testing for Lastname Validation
+        {
+            try
+            {
+                //Act
+                string actual = validation.FirstNameValidation(lastName);
+                //Assert
+                Assert.AreEqual(expected, actual);
+            }
+            catch (InvalidException ex) // catch exception if input is not valid or null or empty
+            {
+                Assert.AreEqual(expected, ex.Message);
+            }
+        }
     }
 }
--- UserRegistrationWithExceptionAndReflaction/Program.cs
+++ UserRegistrationWithExceptionAndReflaction/Program.cs
@@ -21,6 +21,15 @@ namespace UserRegistrationWithExceptionAndReflaction
             string checkFirstName = validation.FirstNameValidation(fName); //Calling method to check first name is valid or not with argument
             if(checkFirstName != "Input is valid")//If not valid then ask user to enter first name again
                 goto Firstname;
+
+            // User Last name validation
+            Lastname:
+            Console.WriteLine("\nNote : Your Last name should start with Capital Letter only and has minimum 3 Character and no space between letters");
+            Console.WriteLine("\n\nEnter your last name : ");
+            string lName = Console.ReadLine(); //storing last name entered by user in variable
+            string checkLastName = validation.LastNameValidation(lName); //Calling method to check last name is valid or not with argument
+            if (checkLastName == "Input is valid")//If not valid then ask user to enter last name again
+                goto Lastname;
         }
     }
 }

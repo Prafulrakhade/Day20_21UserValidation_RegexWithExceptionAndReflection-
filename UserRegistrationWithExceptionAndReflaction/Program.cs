77f1b1397031444611fc9a0eda055f19cd1d1ccf
Praful <prafulrakhade02@gmail.com>
2022-05-28 15:10:48 +0530

    [praful] Added : uc_1_Proram file

---
+++ UserRegistrationWithExceptionAndReflaction/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserRegistrationWithExceptionAndReflaction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Validation validation = new Validation(); // Creating a object of Validation class
+            Console.WriteLine("MSTesting");
+            Console.WriteLine("\nWelcome to C# User's Information validation using Regular Expression");
+        //User First name validation            
+        Firstname:
+            Console.WriteLine("\nNote : Your first name should start with Capital Letter only and has minimum 3 Character and no space between letters");
+            Console.WriteLine("\n\nEnter your first name : ");
+            string fName = Console.ReadLine(); //storing first name entered by user in variable
+            string checkFirstName = validation.FirstNameValidation(fName); //Calling method to check first name is valid or not with argument
+            if(checkFirstName != "Input is valid")//If not valid then ask user to enter first name again
+                goto Firstname;
+        }
+    }
+}
---
+++ UserRegistrationWithExceptionAndReflaction/Validation.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace UserRegistrationWithExceptionAndReflaction
+{
+    public class Validation
+    {
+        public string FirstNameValidation(string fName) // Creating a method for First name validation
+        {
+            try
+            {
+                string pattern = "^[A-Z][a-z]{2,}$"; // Regex for first name validation
+                if (fName.Equals(string.Empty))
+                {
+                    throw new InvalidException(InvalidException.ExceptionType.EMPTY_INPUT, "Input should not be empty");
+                }                                     //If name entered by user is match with regex then it is valid otherwise not
+                if (Regex.IsMatch(fName, pattern))
+                {
+                    Console.WriteLine($"\nYour first name \"{fName}\" is valid");
+                    return "Input is valid";
+                }
+                else
+                {
+                    throw new InvalidException(InvalidException.ExceptionType.EMPTY_INPUT, "Input is not valid");
+                }
+
+            }
+            catch (NullReferenceException ex)
+            {
+                throw new InvalidException(InvalidException.ExceptionType.NULL_INPUT, "Input should not be null");
+            }
+            catch (InvalidException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return ex.Message;
+            }
+        }
+    }
+}
